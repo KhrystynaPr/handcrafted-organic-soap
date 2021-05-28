34635d74d19418fe715c7366352699626f9c60ce
KhrystynaPr <christina-seniuta@ukr.net>
2021-05-28 17:49:13 +0300

    fixed alts names

--- js/products.js
+++ js/products.js
@@ -86,9 +86,9 @@ const products = `
 function getProductItemHtml (item) {
   return `
   <article>
-    <a href="product.html"><img src="${item.imgUrl}" alt="illustration" class="soap-image"></a>
+    <a href="product.html"><img src="${item.imgUrl}" alt="Natural soap" class="soap-image"></a>
     <h3><a href="product.html" class="soap-name">${item.name}</a></h3>
-    <p class="soap-price">$${item.price}</p>
+    <p class="soap-price">$ ${item.price}</p>
     <a href="#cart" class="button">Add to Cart &#x27F6;</a>
   </article>
   `;
